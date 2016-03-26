dbb435df3d832dc5be87b44bafefa3277c4ae665
Harry Yang <zillaci@gmail.com>
2016-03-26 18:40:00 +0900

    add Login Activity. draw layout for LoginActivity.

---
+++ app/src/test/java/io/harry/seoulfiesta/activity/LoginActivityTest.java
@@ -0,0 +1,13 @@
+package io.harry.seoulfiesta.activity;
+
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import io.harry.seoulfiesta.BuildConfig;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class)
+public class LoginActivityTest {
+}
